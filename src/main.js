f9e3068c527d3385730e642d6966ba8d848d2341
Ali Eldeba <alieldeba375@gmail.com>
2023-02-05 23:46:01 +0200

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,10 +1,8 @@
-import { createApp, config } from "vue";
+import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
 
 import "./style.css";
-
-config.devtools = true;
 
 const app = createApp(App);
 
