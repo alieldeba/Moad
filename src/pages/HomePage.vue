9e8495e8f8223d5d9c224a80c9b7753c5f784a26
Ali-Eldeba <alieldeba375@gmail.com>
2023-02-05 21:30:54 +0200

    adding starting page

---
+++ src/pages/HomePage.vue
@@ -0,0 +1,10 @@
+<template>
+  <main class="text-center h-screen flex flex-col justify-center bg-red-500">
+    <h1 class="text-3xl leading-relaxed">Welcome To Moad Library</h1>
+    <p class="text-xl">
+      Moad Library Is A Loading UI Library That You Can Use It Easily In Your
+      Website
+    </p>
+    <button class="bg-blue-500 mx-auto py-2 px-5 mt-5">Discover More</button>
+  </main>
+</template>
--- src/router/index.js
+++ src/router/index.js
@@ -3,11 +3,11 @@ import { createRouter, createWebHistory } from "vue-router";
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
-    // {
-    //   path: '/',
-    //   name: 'home',
-    //   component: () => import('../pages/HomeView.vue'),
-    // },
+    {
+      path: "/",
+      name: "home",
+      component: () => import("../pages/HomePage.vue"),
+    },
     // {
     //   path: '/about',
     //   name: 'about',
